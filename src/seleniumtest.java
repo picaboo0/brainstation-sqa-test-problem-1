4a4a4260c0fd2b5dedbad43110b2ea65dc2c4604
Fatematujjohora <fatematujjohora@iamsheba.xyz>
2021-08-24 17:48:25 +0600

    incomplete task 01

--- src/seleniumtest.java
+++ src/seleniumtest.java
@@ -7,12 +7,12 @@ public class seleniumtest {
 	WebDriver driver;
 	public void launchBrowser() 
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mithun\\Desktop\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\shimu\\Desktop\\chromedriver_win32\\chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://automationpractice.com/index.php");
 		driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).click();
-		//driver.findElements(By.xpath("//*[@id=\"email_create\"]")).sendKeys("fshimu.2104@gmail.com");
+		driver.findElements(By.xpath("//*[@id=\"email_create\"]")).sendKeys("fshimu.2104@gmail.com");
 		driver.findElement(By.xpath("//*[@id=\"SubmitCreate\"]/span")).click();
 	}
 	public static void main(String[] args) {
