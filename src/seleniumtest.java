b1d0065d99d96a488dc398467d8be6a301410dac
Fatematujjohora <fatematujjohora@iamsheba.xyz>
2021-08-24 18:31:39 +0600

    incomplete task 01

--- src/seleniumtest.java
+++ src/seleniumtest.java
@@ -7,13 +7,20 @@ public class seleniumtest {
 	WebDriver driver;
 	public void launchBrowser() 
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\shimu\\Desktop\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mithun\\Desktop\\chromedriver_win32\\chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://automationpractice.com/index.php");
 		driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).click();
-		driver.findElements(By.xpath("//*[@id=\"email_create\"]")).sendKeys("fshimu.2104@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"email_create\"]")).sendKeys("fshimu.2104@gmail.com");
 		driver.findElement(By.xpath("//*[@id=\"SubmitCreate\"]/span")).click();
+		driver.findElement(By.xpath("//*[@id=\"account-creation_form\"]/div[1]/div[1]/div[2]/label")).click();
+		driver.findElement(By.xpath("//*[@id=\"customer_firstname\"]")).sendKeys("Fatematujjohora");
+		driver.findElement(By.xpath("//*[@id=\"customer_lastname\"]")).sendKeys("shimu");
+		driver.findElement(By.xpath("//*[@id=\"passwd\"]")).sendKeys("fatema");
+		
+		
+		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
